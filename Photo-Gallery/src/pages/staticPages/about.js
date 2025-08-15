f2d55ff8b2aadd0032a873ea7fda4c0be1072eab
Fenil Parakhiya <fenilparakhiya@Fenils-MacBook-Air.local>
2025-08-15 17:59:50 -0400

    Photo Gallary

---
+++ Photo-Gallery/src/pages/staticPages/about.js
@@ -0,0 +1,13 @@
+// material-ui
+import { Typography } from '@mui/material';
+import MainCard from 'components/MainCard';
+
+const AboutPage = () => {
+  return (<MainCard title="About">
+    <Typography variant="body2">
+      About page for owner of this API!
+    </Typography>
+  </MainCard>
+)};
+
+export default AboutPage;
